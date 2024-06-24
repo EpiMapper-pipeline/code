b13507c2fd8c65de807a4ed13b3b7b05f7e1a90e
Gianna-liu <giannaliu23@gmail.com>
2024-06-24 17:06:33 +0200

    Update dmr_percent2plot.py
    modify the font size

--- EpiMapper/epimapper/function_scripts/dar_scripts/dmr_percent2plot.py
+++ EpiMapper/epimapper/function_scripts/dar_scripts/dmr_percent2plot.py
@@ -14,7 +14,6 @@ def main(count_file_folder, count_file_name,names):
   if not os.path.exists(count_file):
      print('Input count table file is not found please check input file name or path, I stop !', count_file)
      exit(1)
-
   count_data_df=pd.read_csv(count_file,index_col=0,sep='\t')
   count_data_matrix=count_data_df.to_numpy()
   percent_up=count_data_matrix[:,1]/count_data_matrix[:,0]*100
@@ -24,25 +23,31 @@ def main(count_file_folder, count_file_name,names):
   percent_data_matrix=np.concatenate((percent_up.reshape(1,data_size),percent_down.reshape(1,data_size)),axis=0)
   percent_data_df=pd.DataFrame(data=percent_data_matrix.T,columns=['up_percent','down_percent'],index=count_data_df.index.to_list())
   percent_data_df['genome']=percent_data_df.index.to_list()
-  ax=percent_data_df.plot.bar(x='genome',y=['up_percent','down_percent'], title=title_name ,figsize=(6,8))
-  ax.set_ylabel('Percentage')
+  ax=percent_data_df.plot.bar(x='genome',y=['up_percent','down_percent'], title=title_name ,figsize=(18,24)) # font size
+  ax.set_title(title_name, fontsize=42, fontweight='bold')
+  ax.tick_params(axis='x', labelsize=32) #axis font
+  ax.tick_params(axis='y', labelsize=32)#axis font
+  ax.set_xlabel('',fontdict={'fontsize': 37, 'weight': 'bold'}) # ylabel font size
+  ax.set_ylabel('Percentage',fontdict={'fontsize': 37, 'weight': 'bold'}) # ylabel font size
+  ax.legend(fontsize=37) # legend font size
   out_fig_file=count_file.replace('.csv','.pdf')
   out_fig_file=out_fig_file.replace("differential_analysis/out_combined_files/genome","summary_tables")
+
   #out_fig_file=out_fig_file.replace('counts','percent')
   #print(out_fig_file)
+
   plt.savefig(out_fig_file, format='pdf')
+
+  # save png
+  out_fig_file2=out_fig_file.replace('.pdf','.png')
+  plt.savefig(out_fig_file2, format='png', dpi=300)
+  print("finished Epimapper_DAR_ttest_pval_0.01.png")
   #plt.show()
 
 def dmr_percent2plot_main(in_countFile_folder,in_countFile_name, names):
   count_file_folder=in_countFile_folder
   count_file_name= in_countFile_name
   main(count_file_folder, count_file_name, names)
-
-
-
- 
-
-
 
 
 
